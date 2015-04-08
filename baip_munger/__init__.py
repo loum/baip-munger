3bd0ff97258c16e41623dcb37d4f12f03245aa64
Lou Markovski <lou.markovski@gmail.com>
2015-04-08 01:49:53 +0000

    Tidyup namespace imports

--- baip_munger/__init__.py
+++ baip_munger/__init__.py
@@ -1,4 +1,4 @@
 """Support shorthand import of our classes into the namespace.
 """
-from baip_munger.munger import Munger
-from baip_munger.xpathgen import XpathGen
+from munger import Munger
+from xpathgen import XpathGen
